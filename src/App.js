9c641b60cdc9066b0f6c522d5d3f41b59d4d4eb8
Alan Juarez Fragoso <alaanjuarezf@gmail.com>
2023-08-14 22:39:44 -0600

    handleSubmit categories

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ import {Calif} from './components/Calif';
 import {New} from './components/New';
 import {Index_buildings} from './components/buildings/Index_buildings';
 import {Register_buildings} from './components/buildings/Register_buildings';
+import {Index_categories} from './components/categories/Index_categories';
+import {Register_categories} from './components/categories/Register_categories';
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
 import axios from 'axios';
 import { Apiurl } from './services/apirest';
@@ -81,6 +83,10 @@ function App() {
                     <Route path="/profile" element={<Profile />} />
                     <Route path="/new" element={<New />} />
                     <Route path="/calif" element={<Calif />} />
+                    <Route path="/categories/*" element={<Routes>
+                      <Route path="/" element={<Index_categories />} />
+                      <Route path="/register" element={<Register_categories />} />
+                    </Routes>}/>
                     <Route path="/buildings/*" element ={<Routes>
                       <Route path="/" element={<Index_buildings />} />
                       <Route path="/register" element={<Register_buildings />} />
