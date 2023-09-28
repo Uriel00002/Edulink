2e5722a4b95c337443c561b14b7958cc6f8d6479
Josue <sjosue5082002@gmail.com>
2023-09-27 21:34:28 -0500

    primeras condiciones

--- src/templates/CRUD.js
+++ src/templates/CRUD.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from './Footer'
 import Header from './Header'
 
 export const CRUD = ({name, fields, handleSubmit, setData, data}) => {
+    const [action, setAction] = useState('ver')
   return (
     <>
        
@@ -13,34 +14,39 @@ export const CRUD = ({name, fields, handleSubmit, setData, data}) => {
             <section className="carreer_building_main row">
                 <div className="col-4 border-end border-dark d-flex flex-column gap-3">
                     <img src="https://via.placeholder.com/300" alt="" />
-                    <button className="btn btn-primary">registrar</button>
-                    <button className="btn btn-primary">Ver</button>
-                    <button className="btn btn-primary">Reportes</button>
+                    <button className="btn btn-primary" onClick={() => setAction('registrar')} >registrar</button>
+                    <button className="btn btn-primary" onClick={() => setAction('ver')}>Ver</button>
+                    <button className="btn btn-primary" onClick={() => setAction('reportes')}>Reportes</button>
                 </div>
 
 
-                <form className="col-8" onSubmit={handleSubmit}>
+                {
+                    action === 'registrar' &&
+                    <form className="col-8" onSubmit={handleSubmit}>
 
-                    {
-                        fields?.map((field, index) => {
-                            return (
-                                <div key={index}>
-                                    <label htmlFor={field.name}>{field.verbose}</label>
-                                    <input type={
-                                        field.type === "DateField" ? 'date' : 'text'
-                                    } name={field.name} id={field.name} onChange={(e) => setData({
-                                        ...data,
-                                        form: {
-                                            ...data.form,
-                                            [field.name]: e.target.value
-                                        }
-                                    })} />
-                                </div>
-                            )
-                        })
-                    }
-                    <button type='submit'>Enviar</button>
-                </form>
+                        {
+                            fields?.map((field, index) => {
+                                return (
+                                    <div key={index}>
+                                        <label htmlFor={field.name}>{field.verbose}</label>
+                                        <input type={
+                                            field.type === "DateField" ? 'date' : 'text'
+                                        } name={field.name} id={field.name} onChange={(e) => setData({
+                                            ...data,
+                                            form: {
+                                                ...data.form,
+                                                [field.name]: e.target.value
+                                            }
+                                        })} />
+                                    </div>
+                                )
+                            })
+                        }
+                        <button type='submit'>Enviar</button>
+                    </form>
+                }
+                {action === 'ver' && <div></div>}
+                {action === 'reportes' && <div></div>}
 
             </section>
 
