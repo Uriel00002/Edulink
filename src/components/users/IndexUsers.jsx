2b4b7a459c2866b30c789d92db17730acb1ce769
Josue <sjosue5082002@gmail.com>
2023-10-16 19:41:40 -0500

    mejora en estilos de seccion de usuarios

--- src/components/users/IndexUsers.jsx
+++ src/components/users/IndexUsers.jsx
@@ -6,41 +6,62 @@ import '../../assets/css/list.css'
 
 export const IndexUsers = () => {
   const navigate = useNavigate()
+  const list = [
+    {
+      name: 'Cuentas',
+      link: [{name: 'Usuarios', link: 'users'}],
+    },
+    {
+      name: 'Estudiantes',
+      link: [
+        {name: 'Datos academicos', link: 'students'},
+        {name: 'Preparatorias', link: 'highschools'},
+        {name: 'Perfiles', link: 'profiles'},
+        {name: 'Direcciones', link: 'addresses'},
+      ],
+    },
+    {
+      name: 'Padres',
+      link: [{name: 'Datos familiares', link: 'parents'}],
+    },
+    {
+      name: 'Empleados',
+      link: [
+        {name: 'Datos laborales', link: 'employees'},
+        {name: 'Puestos', link: 'positions'},
+      ],
+    }
+  ]
   return (
     <>
        <section className="header_main">
                 <Header name={"Usuarios"} />
             </section>
             <section className="dash_main">
-                <div className="dash_menu">
-                <div class="list-multiple">
-                  <ul>
-                    <li class="parent">Cuentas
-                      <ul class="sub-list">
-                        <li onClick={() => navigate('users')}>Usuarios</li>
-                      </ul>
-                    </li>
-                    <li class="parent">Estudiantes
-                      <ul class="sub-list">
-                        <li onClick={() => navigate('students')}>Datos academicos</li>
-                        <li onClick={() => navigate('highschools')}>Preparatorias</li>
-                        <li onClick={() => navigate('profiles')}>Perfiles</li>
-                        <li onClick={() => navigate('addresses')}>Direcciones</li>
-                      </ul>
-                    </li>
-                    <li class="parent">Padres
-                      <ul class="sub-list">
-                        <li onClick={() => navigate('parents')}>Datos familiares</li>
-                      </ul>
-                    </li>
-                    <li class="parent">Empleados
-                      <ul class="sub-list">
-                        <li onClick={() => navigate('employees')}>Datos laborales</li>
-                        <li onClick={() => navigate('positions')}>Puestos</li>
-                      </ul>
-                    </li>
-                  </ul>
-                </div>
+                <div className="dash_menu row gap-5">
+                  {
+                    list.map((item, index) => {
+                      return (
+                        <div key={index} className="item_list col-md-2">
+                          <h3 onClick={(e) =>{
+                            document.querySelectorAll('.active').forEach((item) => {
+                              item.classList.remove('active')
+                            })
+                            e.target.classList.toggle('active')
+                          }}>{item.name}</h3>
+                          <ul>
+                            {
+                              item.link.map((item, index) => {
+                                return (
+                                  <li key={index} onClick={() => navigate(item.link)}>{item.name}</li>
+                                )
+                              })
+                            }
+                          </ul>
+                        </div>
+                      )
+                    })
+                  }
                 </div>
             </section>
             <section className="footer_main">
