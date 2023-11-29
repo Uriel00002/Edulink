83bf74c54444358a640b4abc69587a257abe7529
leiru <uriellopezaquino1.0@gmail.com>
2023-11-28 20:17:05 -0600

    Rediseño de vista error404

--- src/components/Error404.jsx
+++ src/components/Error404.jsx
@@ -10,29 +10,33 @@ export const Error404 = () => {
   const width = window.innerWidth
 
   return (
-    <div className='main_error d-flex flex-column w-100 align-items-center'>
-      <h1 className='msg_error'>ERROR 404</h1>
-      <Link to='/' className='title_error'>Inicio</Link>
-      <button onClick={() => window.history.go(-2)} className='btn_back'>Regresar a la página anterior</button>
-      <button onClick={() => setGame(!game)} className='btn_play'>Jugar</button>
-      {
-        game && (width < 800 || height < 600) &&
-        <div className="w-100 alert alert-warning">
-          No se puede mostrar el juego, las dimensiones de la pantalla no son suficientes
-        </div>
-      }
-      {
-        game && width > 800 && height > 600 &&
-        <div className="w-100">
-          <iframe
-          src="https://oojosueoo.github.io/laberinto_unity/"
-          frameBorder="0"
-          class="giphy-embed"
-          allowFullScreen
-          style={{ width: '100%', height: '100vh' }}
-        ></iframe>
-        </div>
-      }
-    </div>
+    <section className='main_error row row-cols-1 d-flex  align-items-center'>
+      <div className="game col col-10" >
+        <button onClick={() => window.history.go(-2)} className='btn_back'>REGRESAR</button>
+        <button onClick={() => setGame(!game)} className='btn_play'>JUGAR</button>
+        <Link to='/' className='title_error'>INICIO</Link>
+        {
+          game && (width < 800 || height < 600) &&
+          <div className="w-100 alert alert-warning">
+            No se puede mostrar el juego, las dimensiones de la pantalla no son suficientes
+          </div>
+        }
+        {
+          game && width > 800 && height > 600 &&
+          <div className="w-100">
+            <iframe
+              src="https://oojosueoo.github.io/laberinto_unity/"
+              frameBorder="0"
+              class="giphy-embed"
+              allowFullScreen
+              style={{ width: '100%', height: '100vh' }}
+            ></iframe>
+          </div>
+        }
+      </div>
+      <div className="error col col-2" >
+        <h1 className='msg_error'>ERROR 404</h1>
+      </div>
+    </section>
   )
 }
